6cd626a22c24a493c24c93a2febe4b85d7658fa0
Katiuska <katiuskaalicea@gmail.com>
2020-11-14 22:20:22 -0800

    plugged index.js to store imported middlewareand i am implementing it in the createStore function

--- my-redux-app/src/index.js
+++ my-redux-app/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { reducer } from "./reducers";
+import logger from "react-logger";
+import thunk from "react-thunk";
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
 
