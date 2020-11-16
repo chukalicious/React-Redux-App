cf2fd2765af06b6a765b9b8af381761ef9b3a052
Katiuska <katiuskaalicea@gmail.com>
2020-11-15 18:17:57 -0800

    when an error occures there nothing to notify the user of it.

--- my-redux-app/src/App.js
+++ my-redux-app/src/App.js
@@ -1,11 +1,9 @@
-/* import "./App.css"; */
+import "./App.css";
 import Characters from "./components/Characters";
 
 function App() {
   return (
-    <div
-    // className="App"
-    >
+    <div className="App">
       <Characters />
     </div>
   );
