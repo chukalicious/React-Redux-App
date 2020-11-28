aaac5d1fb56258bd3d1d5536f4782b1386f84c86
Katiuska <katiuskaalicea@gmail.com>
2020-11-27 17:50:48 -0800

    fixed error preventing the app to show in local host. I was routing to App.js ion that same file

--- my-redux-app/src/App.js
+++ my-redux-app/src/App.js
@@ -1,15 +1,26 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import Characters from "./components/Characters";
 import Heading from "./components/Heading";
 import CharacterDetails from "./components/CharacterDetails";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Heading />
+      <Switch>
+        <Route path="/characters" render={() => <Characters />} />
+        <Route path="/" component={Home} />
+      </Switch>
 
-      <Characters />
-      <CharacterDetails />
+      {/* <Switch> */}
+      {/* <Route path="/characters/:id">
+        <CharacterDetails />
+      </Route> */}
+      {/* <Route path="/characters/" render={(props) => <Characters />} /> */}
+      {/* <Route path="/" component={App} /> */}
+      {/* </Switch> */}
     </div>
   );
 }
