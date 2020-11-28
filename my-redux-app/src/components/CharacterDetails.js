05085779acfe582b458ce8d923788dd517718876
Katiuska <katiuskaalicea@gmail.com>
2020-11-27 20:17:42 -0800

    added more meat to the CharacterDetails component

--- my-redux-app/src/components/CharacterDetails.js
+++ my-redux-app/src/components/CharacterDetails.js
@@ -9,6 +9,11 @@ const CharacterDetails = (props) => {
   return (
     <div>
       <h2>{props.details.name} details component</h2>
+      <p>Role: {props.details.role}</p>
+      <p>Voiced By: </p>
+      {props.details.voice_actors.map((voice) => (
+        <p>{voice.name}</p>
+      ))}
     </div>
   );
 };
